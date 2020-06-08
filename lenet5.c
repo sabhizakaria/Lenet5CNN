20322c63489c7486d8bbb1b72d6a2edada98f34d
Sabhi Zakaria <sabhizakaria@gmail.com>
2020-06-08 13:40:40 +0200

    finishing second layer of conv

--- lenet5.c
+++ lenet5.c
@@ -124,20 +124,17 @@ void conv1_2(int input [nb_layer][outputH_1p][outputW_1p], int kernel [nb_filter
 	int output [nb_filter_conv_2][outputH_2c][outputW_2c]){
 
 	int nb_filtre,nb_l,i, j, k, l, res = 0;
-	for (nb_l = 0 ; nb_l < 6 ; nb_l++){
+	for (nb_l = 0 ; nb_l < nb_layer ; nb_l++){
 		for (nb_filtre = 0 ; nb_filtre < nb_filter_conv_2; nb_filtre++){
-			for (i = 0 ; i < outputH_2c ; i+=stride){
-				 
+			for (i = 0 ; i < outputH_2c ; i+=stride){				 
 					if (nb_filtre = 0) { // (featur map)
-						if (nb_l == 0 || nb_l == 1 ||nb_l == 2 ){
+						if (nb_l == 0 || nb_l == 1 ||nb_l == 2 ){ // (nb of Layer)
 							// Get the conv result 
 							for ( k = 0 ; k < kernel_H ; k++){ // kernel dimention 
 								for ( l = 0 ; l < kernel_W; l++) {
 									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
 								}
 							}
-							output[nb_filtre][i][j] = relu_func(res) ;	
-							res = 0 ;
 						}
 					}
 
@@ -163,9 +160,6 @@ void conv1_2(int input [nb_layer][outputH_1p][outputW_1p], int kernel [nb_filter
 									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
 								}
 							}
-							output[nb_filtre][i][j] = relu_func(res) ;	
-							res = 0 ;
-
 						}
 					}
 
@@ -177,9 +171,6 @@ void conv1_2(int input [nb_layer][outputH_1p][outputW_1p], int kernel [nb_filter
 									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
 								}
 							}
-							output[nb_filtre][i][j] = relu_func(res) ;	
-							res = 0 ;
-
 						}
 					}
 					if (nb_filtre = 4) { // (featur map)
@@ -190,9 +181,6 @@ void conv1_2(int input [nb_layer][outputH_1p][outputW_1p], int kernel [nb_filter
 									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
 								}
 							}
-							output[nb_filtre][i][j] = relu_func(res) ;	
-							res = 0 ;
-
 						}
 					}
 					if (nb_filtre = 5) { //(featur map)
@@ -203,9 +191,6 @@ void conv1_2(int input [nb_layer][outputH_1p][outputW_1p], int kernel [nb_filter
 									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
 								}
 							}
-							output[nb_filtre][i][j] = relu_func(res) ;	
-							res = 0 ;
-
 						}
 					}
 					if (nb_filtre = 6) { // (featur map)
@@ -216,25 +201,120 @@ void conv1_2(int input [nb_layer][outputH_1p][outputW_1p], int kernel [nb_filter
 									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
 								}
 							}
-							output[nb_filtre][i][j] = relu_func(res) ;	
-							res = 0 ;
-
 						}
 					}
 
 					if (nb_filtre = 6) { // (featur map)
-						if (i == 0 || i == 1 ||i == 2 || i == 3){
+						if (nb_l == 0 || nb_l == 1 ||nb_l == 2 || nb_l == 3){
 							// Get the conv result 
 							for ( k = 0 ; k < kernel_H ; k++){ // kernel dimention 
 								for ( l = 0 ; l < kernel_W; l++) {
 									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
 								}
 							}
-							output[nb_filtre][i][j] = relu_func(res) ;	
-							res = 0 ;
 
 						}
 					}
+
+					if (nb_filtre = 7) { // (featur map)
+						if (nb_l == 1 || nb_l == 2 ||nb_l == 3 || nb_l == 4){
+							// Get the conv result 
+							for ( k = 0 ; k < kernel_H ; k++){ // kernel dimention 
+								for ( l = 0 ; l < kernel_W; l++) {
+									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
+								}
+							}
+						}
+					}
+
+					if (nb_filtre = 8) { // (featur map)
+						if (nb_l == 2 || nb_l == 3 ||nb_l == 4 || nb_l == 5){
+							// Get the conv result 
+							for ( k = 0 ; k < kernel_H ; k++){ // kernel dimention 
+								for ( l = 0 ; l < kernel_W; l++) {
+									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
+								}
+							}
+						}
+					}
+
+					if (nb_filtre = 9) { // (featur map)
+						if (nb_l == 3 || nb_l == 4 ||nb_l == 5 || nb_l == 0){
+							// Get the conv result 
+							for ( k = 0 ; k < kernel_H ; k++){ // kernel dimention 
+								for ( l = 0 ; l < kernel_W; l++) {
+									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
+								}
+							}
+						}
+					}
+
+					if (nb_filtre = 10) { // (featur map)
+						if (nb_l == 4 || nb_l == 5 ||nb_l == 0 || nb_l == 1){
+							// Get the conv result 
+							for ( k = 0 ; k < kernel_H ; k++){ // kernel dimention 
+								for ( l = 0 ; l < kernel_W; l++) {
+									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
+								}
+							}
+						}
+					}
+
+					if (nb_filtre = 11) { // (featur map)
+						if (nb_l == 5 || nb_l == 0 ||nb_l == 1 || nb_l == 2){
+							// Get the conv result 
+							for ( k = 0 ; k < kernel_H ; k++){ // kernel dimention 
+								for ( l = 0 ; l < kernel_W; l++) {
+									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
+								}
+							}
+						}
+					}
+
+					if (nb_filtre = 12) { // (featur map)
+						if (nb_l == 0 || nb_l == 1 ||nb_l == 2 || nb_l == 4){
+							// Get the conv result 
+							for ( k = 0 ; k < kernel_H ; k++){ // kernel dimention 
+								for ( l = 0 ; l < kernel_W; l++) {
+									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
+								}
+							}
+						}
+					}
+
+					if (nb_filtre = 13) { // (featur map)
+						if (nb_l == 1 || nb_l == 2 ||nb_l == 4 || nb_l == 5){
+							// Get the conv result 
+							for ( k = 0 ; k < kernel_H ; k++){ // kernel dimention 
+								for ( l = 0 ; l < kernel_W; l++) {
+									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
+								}
+							}
+						}
+					}
+
+					if (nb_filtre = 14) { // (featur map)
+						if (nb_l == 0 || nb_l == 2 ||nb_l == 3 || nb_l == 5){
+							// Get the conv result 
+							for ( k = 0 ; k < kernel_H ; k++){ // kernel dimention 
+								for ( l = 0 ; l < kernel_W; l++) {
+									res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
+								}
+							}
+						}
+					}
+					if (nb_filtre = 15) { // (featur map)
+						// Get the conv result 
+						for ( k = 0 ; k < kernel_H ; k++){ // kernel dimention 
+							for ( l = 0 ; l < kernel_W; l++) {
+								res = res + input[nb_l][k+i][l+j] * kernel[nb_filtre][k][l];
+							}
+						}					
+					}
+
+					output[nb_filtre][i][j] = relu_func(res) ;	
+					res = 0 ;
+
 
 
 			}
